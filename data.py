c7d97ae562c19e484cddd02b60e64cc1649acc18
Abdulsalam Ali <abdulsalamali1997@gmail.com>
2025-06-07 01:05:45 +0300

    Update data.py

--- data.py
+++ data.py
@@ -1,29 +1,34 @@
-
 import gspread
 import pandas as pd
 from google.oauth2.service_account import Credentials
 import json
 import streamlit as st
 
+# Google Sheets API setup
 SCOPE = ["https://www.googleapis.com/auth/spreadsheets"]
 creds_dict = json.loads(st.secrets["GOOGLE_CREDENTIALS"])
 creds = Credentials.from_service_account_info(creds_dict, scopes=SCOPE)
 client = gspread.authorize(creds)
+
+# Your main spreadsheet
 SPREADSHEET_ID = "1hwVsrPQjJdv9c4GyI_QzujLzG3dImlUHxmOUbUdjY7M"
 sheet = client.open_by_key(SPREADSHEET_ID)
 
+# Inventory functions
 def load_inventory():
     ws = sheet.worksheet("Inventory")
     data = ws.get_all_records()
     df = pd.DataFrame(data)
     return df
 
+# Clients functions
 def load_clients():
     ws = sheet.worksheet("Clients")
     data = ws.get_all_records()
     df = pd.DataFrame(data)
     return df
 
+# Sales functions
 def load_sales():
     ws = sheet.worksheet("Sales")
     data = ws.get_all_records()
