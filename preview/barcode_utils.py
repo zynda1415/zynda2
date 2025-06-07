cc4d795c21be10582a82f04ef680cf0d586330b8
Abdulsalam Ali <abdulsalamali1997@gmail.com>
2025-06-08 01:48:57 +0300

    Add files via upload

---
+++ preview/barcode_utils.py
@@ -0,0 +1,32 @@
+import io
+import base64
+from PIL import Image
+from barcode import Code128
+from barcode.writer import ImageWriter
+import qrcode
+
+def generate_barcode_image(code_value, dpi=300):
+    barcode_io = io.BytesIO()
+    options = {
+        'module_width': 0.3,
+        'module_height': 20,
+        'font_size': 8,
+        'text_distance': 1,
+        'dpi': dpi  # ✅ full dpi support
+    }
+    Code128(code_value, writer=ImageWriter()).write(barcode_io, options)
+    barcode_io.seek(0)
+    return Image.open(barcode_io)
+
+def generate_qr_code(code_value):
+    qr = qrcode.QRCode(box_size=2, border=1)
+    qr.add_data(code_value)
+    qr.make(fit=True)
+    img = qr.make_image(fill_color="black", back_color="white")
+    return img
+
+def encode_image_to_base64(img):
+    buffer = io.BytesIO()
+    img.save(buffer, format="PNG")
+    b64_img = base64.b64encode(buffer.getvalue()).decode()
+    return b64_img
