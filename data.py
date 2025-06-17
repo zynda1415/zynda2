504a4382f9da62b0e775f84ea813e977418fcdc1
Abdulsalam Ali <abdulsalamali1997@gmail.com>
2025-06-17 21:45:50 +0300

    Update data.py

--- data.py
+++ data.py
@@ -1,49 +1,43 @@
 import streamlit as st
-import gspread
 import pandas as pd
 import json
-from google.oauth2.service_account import Credentials
+import pygsheets
 
 # === Setup ===
 SPREADSHEET_ID = "1hwVsrPQjJdv9c4GyI_QzujLzG3dImlUHxmOUbUdjY7M"
-scope = ["https://www.googleapis.com/auth/spreadsheets"]
 
 @st.cache_data(ttl=60)
 def connect_sheet():
     creds_dict = json.loads(st.secrets["GOOGLE_CREDENTIALS"])
-    creds = Credentials.from_service_account_info(creds_dict, scopes=scope)
-    client = gspread.authorize(creds)
+    client = pygsheets.authorize(service_account_info=creds_dict)
     return client.open_by_key(SPREADSHEET_ID)
 
 # === Inventory ===
 def load_inventory_data():
-    sheet = connect_sheet().worksheet("Items")
-    data = sheet.get_all_records()
-    return pd.DataFrame(data)
+    sheet = connect_sheet().worksheet_by_title("Items")
+    return sheet.get_as_df()
 
 def save_inventory_data(df):
-    sheet = connect_sheet().worksheet("Items")
+    sheet = connect_sheet().worksheet_by_title("Items")
     sheet.clear()
-    sheet.update([df.columns.values.tolist()] + df.values.tolist())
+    sheet.set_dataframe(df, start=(1, 1))
 
 # === Invoices ===
 def load_invoice_data():
-    sheet = connect_sheet().worksheet("Invoices")
-    data = sheet.get_all_records()
-    return pd.DataFrame(data)
+    sheet = connect_sheet().worksheet_by_title("Invoices")
+    return sheet.get_as_df()
 
 def save_invoice_data(df):
-    sheet = connect_sheet().worksheet("Invoices")
+    sheet = connect_sheet().worksheet_by_title("Invoices")
     sheet.clear()
-    sheet.update([df.columns.values.tolist()] + df.values.tolist())
+    sheet.set_dataframe(df, start=(1, 1))
 
 # === Clients ===
 def load_clients_data():
-    sheet = connect_sheet().worksheet("Clients")
-    data = sheet.get_all_records()
-    return pd.DataFrame(data)
+    sheet = connect_sheet().worksheet_by_title("Clients")
+    return sheet.get_as_df()
 
 def save_clients_data(df):
-    sheet = connect_sheet().worksheet("Clients")
+    sheet = connect_sheet().worksheet_by_title("Clients")
     sheet.clear()
-    sheet.update([df.columns.values.tolist()] + df.values.tolist())
+    sheet.set_dataframe(df, start=(1, 1))
